3eab3073467981e589877ab7b65b9c0665d0759b
sunil <sunil.shastry69@gmail.com>
2025-03-12 22:12:18 -0600

    Document with jsdocs for greet function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Greet users with custom greeting, name and logging functionality
+ * @param {string} greeting Greeting message to be used
+ * @param {string} username User's name
+ * @param {boolean} log Toggle logging functionality
+ * @returns
+ */
 function greet(greeting = `Hello there`, username = 'user', log = false) {
 	const output = `${greeting}, ${username}!`;
 	if (log) {
